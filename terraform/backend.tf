bb5afb5b991656a662e91a67d0cb80d4ea9850b8
Brady, Luke <luke.brady@pearson.com>
2020-01-18 14:01:21 -0500

    Adding updated backend and version.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "ue4-build-tf-state-$${var.region}"
+    bucket = "ue4-build-tf-state"
     key    = "terraform.tfstate"
     region = "us-east-1"
+    profile  = "test_sdk"
+    dynamodb_table = "terraform-locks"
   }
 }
 
--- terraform/build-ue4-server.tf
+++ terraform/build-ue4-server.tf
@@ -9,9 +9,9 @@ variable "region" {
 
 provider "aws" {
   version    = "~> 2.43"
-  region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
+  region     = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
 
 module "ue4-aws" {
